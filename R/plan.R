0f04b523d484a4b353ea9b8cf7c305ec6d4c5a20
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2020-09-29 05:01:27 +0200

    finished pipeline

--- R/plan.R
+++ R/plan.R
@@ -1,28 +1,28 @@
-# also exclude files with underscore
-
-# https://rdrr.io/github/webbedfeet/coursedown/src/inst/templates/_drake.R
-
-
-# pages_paths <- fs::dir_ls("reports/", glob = "*.Rmd")
-
+# needs to be outside of the drake plan for `rmd_files = !!pages_paths` to work
 pages_paths <- dir("reports", pattern = "*.Rmd", full.names = TRUE)
+# TODO: exclude _file.Rmd with regex in dir
 
-# pages_paths <- c("reports/analysis.Rmd", "reports/dataset.Rmd",
-#                 "reports/dependencies.Rmd", "reports/descriptives.Rmd",
-#                 "reports/index.Rmd")
 plan <- drake_plan(
-
-  # TODO: Trigger dependency on _site.yml
-  # TODO: exclude _file.Rmd with regex in dir
   render_pages = target(
+    # TODO: Add a trigger dependency on _site.yml
     command = rmarkdown::render(
       knitr_in(rmd_files),
-      output_dir = file_out("docs/"),
+      # output_yaml = "reports/_site.yml",
+      # output_options = NULL,
+      output_dir = file_out("docs_pages/"),
       quiet = TRUE
     ),
-    transform = map(rmd_files = !!pages_paths) # the `!!` does it all but I don't know how and why
+    transform = map(rmd_files = !!pages_paths)
+    # “bang-bang” operator !! from tidy evaluation (very important!)
+  ),
+  
+  render_site = target(
+    command = rmarkdown::render_site(
+      input = file_in("reports"),
+      quiet = TRUE
+    )
   )
 )
 
-# plot(plan)
-# vis_drake_graph(plan)
+
+# not quite sure about the site_libs dir of render_site()...
--- _drake.R
+++ _drake.R
@@ -1,26 +1,44 @@
 ## Load your R files
 lapply(list.files("./R", full.names = TRUE), source)
 
-## Update Project
-drake_config(
-  plan,
-  targets = NULL,             # specify targets
-  verbose = 1L,               # show progress bar (1L seems most useful)
-  jobs = 1L,                  # allow n parallel jobs
-  jobs_preprocess = 1L,
-  retries = 0,                # optionally allow n retries
-  log_progress = FALSE,       # log progress of target building in /.drake
-  trigger = drake::trigger(), # optionally edit triggers in trigger function
-  skip_targets = FALSE,       # optionally just load files
-  skip_imports = FALSE,       # optionally just test plan
-  keep_going = FALSE,         # stop make if one target fails
-  garbage_collection = FALSE,
-  memory_strategy = "speed",  # set to "autoclean" and set garbage-colelction to TRUE to safe working memory
-  history = FALSE,            # set to true for saving make() builds in /.drake
-  log_build_times = FALSE,    # Mac users may notice a 20% speedup in make() with build_times = FALSE
-  format = NULL,
-  lock_cache = TRUE,          # However, if you interrupt make() before it can clean itself up, then the cache will stay locked, and you will need to manually unlock it with drake::drake_cache("xyz")$unlock()
+
+
+
+# HCP Settings
+options(
+  clustermq.scheduler = "sge",
+  clustermq.template = "sge_clustermq.tmpl"
 )
+
+## Minimal Config
+drake_config(plan,
+             jobs = 4,
+             parallelism = "clustermq",
+             console_log_file = "drake.log",
+             )
+
+
+## Full Config
+# drake_config(
+#   plan,
+#   targets = NULL,             # specify targets
+#   verbose = 1L,               # show progress bar (1L seems most useful)
+#   jobs = 1L,                  # allow n parallel jobs
+#   jobs_preprocess = 1L,
+#   retries = 0,                # optionally allow n retries
+#   log_progress = FALSE,       # log progress of target building in /.drake
+#   trigger = drake::trigger(), # optionally edit triggers in trigger function
+#   skip_targets = FALSE,       # optionally just load files
+#   skip_imports = FALSE,       # optionally just test plan
+#   keep_going = FALSE,         # stop make if one target fails
+#   garbage_collection = FALSE,
+#   memory_strategy = "speed",  # set to "autoclean" and set garbage-colelction to TRUE to safe working memory
+#   history = TRUE,             # set to true for saving make() builds in /.drake
+#                               ## use with `drake_history(analyze = TRUE)`
+#   log_build_times = FALSE,    # Mac users may notice a 20% speedup in make() with build_times = FALSE
+#   format = NULL,
+#   lock_cache = TRUE,          # However, if you interrupt make() before it can clean itself up, then the cache will stay locked, and you will need to manually unlock it with drake::drake_cache("xyz")$unlock()
+# )
 
 
 
